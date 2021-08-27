9d2dc071b8ad3276c6d862d61955fdefdcef52d6
marigut <mari.gutierrez96@yahoo.com>
2021-08-26 21:34:47 -0500

    questions:

---
+++ assets/js/script.js
@@ -0,0 +1,45 @@
+
+var quizArray = [
+  {
+      q:'What kind of statement is used to execute actions base on a trigger or condition?',
+      a:'Conditional Statement',
+      b:'Fired Event',
+      c:'Regular Expression',
+      d:'For Loop',
+      correctAnswer:'a'
+  },
+  {
+      q:'very useful tool used during development and debugging for printing content to the debugger is?',
+      a:'terminal/bash',
+      b:'JS',
+      c:'CSS',
+      d:'Jfor loops',
+      correctAnswer:'d'
+  },
+  {
+      q:'What tag is used to define a standard cell inside a table?',
+      a:'<h1> to <h6>',
+      b:'<footer',
+      c:'"<button>',
+      d:'<td>',
+      correctAnswer:'d'
+  },
+  {
+      q:'What method you can use to convert a content to strings before to save in the local storage?',
+      a:'localStorage.setItem()',
+      b:'JSON.stringify()',
+      c:'JOHN.stringify()',
+      d:'I command you to save this thing as string',
+      correctAnswer:'b'
+  },
+  {
+      q:'Common data types do not include:',
+      a:'booleans',
+      b:'alerts',
+      c:'numbers',
+      d:'strings',
+      correctAnswer:'b'
+  }
+];
+
+
